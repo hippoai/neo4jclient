71b53caaa489aab606565aab3c17b7f2840a4bc3
selim <selim@wearkinetic.com>
2017-01-20 20:14:49 -0500

    compare a raw cypher query with a neo4jclient statement

---
+++ test_helper.go
@@ -0,0 +1,47 @@
+package neo4jclient
+
+import (
+	"fmt"
+	"strings"
+)
+
+// compressQuery removes spaces and blank lines
+func compressQuery(q string) string {
+	q1 := strings.Replace(q, " ", "", -1)
+	q2 := strings.Replace(q1, "\n", "", -1)
+	return q2
+}
+
+// insertProp replaces a property placeholder by its value
+func insertProp(q, key string, value interface{}) string {
+	valueStr, isString := value.(string)
+	if isString {
+		return strings.Replace(
+			q,
+			fmt.Sprintf("{props}.%s", key),
+			fmt.Sprintf("\"%s\"", valueStr),
+			-1,
+		)
+	} else {
+		return strings.Replace(
+			q,
+			fmt.Sprintf("{props}.%s", key),
+			fmt.Sprintf("%v", value),
+			-1,
+		)
+	}
+}
+
+// IsSameQuery assesses whether two queries are the same
+func IsSameQuery(expected string, statement *Statement) bool {
+	cExpected := compressQuery(expected)
+	cOriginal := compressQuery(statement.Statement)
+
+	if statement.Parameters.Props != nil {
+		for key, value := range statement.Parameters.Props {
+			cOriginal = insertProp(cOriginal, key, value)
+		}
+	}
+
+	return cExpected == cOriginal
+}
