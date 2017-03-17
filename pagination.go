c865ae41f5b51c6f409c49e288e013f4f1bdf4cc
selim <selim@wearkinetic.com>
2017-03-17 13:26:26 -0400

    Tested return count, skip, limit

--- pagination.go
+++ pagination.go
@@ -7,6 +7,7 @@ import (
 
 // AddAtTheEnd adds a string at the end of the Cypher query
 func (s *Statement) AddAtTheEnd(addMe string) *Statement {
+	s.clean()
 	s.Cypher = fmt.Sprintf("%s\n%s", s.Cypher, addMe)
 	return s
 }
@@ -32,10 +33,23 @@ func (s *Statement) AddSkipAndLimit(skip, limit int) *Statement {
 		AddAtTheEnd(fmt.Sprintf("LIMIT %d", limit))
 }
 
+func (s *Statement) clean() {
+	cypherRows := strings.Split(s.Cypher, "\n")
+	newRows := []string{}
+	for _, cypherRow := range cypherRows {
+		if strings.Replace(cypherRow, " ", "", -1) == "" {
+			continue
+		}
+		newRows = append(newRows, cypherRow)
+	}
+	s.Cypher = strings.Join(newRows, "\n")
+}
+
 // OnlyReturnACount breaks down the query row by row
 // removes the previous RETURN if there was one on the last line
 // and adds a count for the given "countMe" variable
 func (s *Statement) OnlyReturnACount(countMe string) *Statement {
+	s.clean()
 	cypherRows := strings.Split(s.Cypher, "\n")
 
 	cypherCount := fmt.Sprintf("RETURN COUNT(%s) AS _n", countMe)
---
+++ pagination_test.go
@@ -0,0 +1,132 @@
+package neo4jclient
+
+import (
+	"strings"
+	"testing"
+)
+
+func removeWS(s string) string {
+	return strings.Replace(s, " ", "", -1)
+}
+
+func TestAddAtTheEnd(t *testing.T) {
+
+	cypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+  `
+
+	statement := NewStatementNoProps(cypher, "fake query")
+	statement.AddAtTheEnd("I AM HERE")
+
+	newCypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+    I AM HERE
+  `
+
+	s2 := NewStatementNoProps(newCypher, "ok")
+	s2.clean()
+
+	if removeWS(statement.Cypher) != removeWS(s2.Cypher) {
+		t.Fatalf("Expected cypher %s but got %s",
+			removeWS(statement.Cypher),
+			removeWS(s2.Cypher),
+		)
+	}
+
+}
+
+func TestAddOrderBy(t *testing.T) {
+
+	cypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+  `
+
+	statement := NewStatementNoProps(cypher, "fake query")
+	statement.AddOrderBy(false, "d.name", "e.id")
+
+	newCypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+    ORDER BY d.name, e.id DESC
+  `
+
+	s2 := NewStatementNoProps(newCypher, "ok")
+	s2.clean()
+
+	if removeWS(statement.Cypher) != removeWS(s2.Cypher) {
+		t.Fatalf("Expected cypher %s but got %s",
+			removeWS(statement.Cypher),
+			removeWS(s2.Cypher),
+		)
+	}
+
+}
+
+func TestAddSkipAndLimit(t *testing.T) {
+	cypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+  `
+
+	statement := NewStatementNoProps(cypher, "fake query")
+	statement.
+		AddOrderBy(false, "d.name", "e.id").
+		AddSkipAndLimit(100, 10)
+
+	newCypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+    ORDER BY d.name, e.id DESC
+    SKIP 100
+    LIMIT 10
+  `
+
+	s2 := NewStatementNoProps(newCypher, "ok")
+	s2.clean()
+
+	if removeWS(statement.Cypher) != removeWS(s2.Cypher) {
+		t.Fatalf("Expected cypher %s but got %s",
+			removeWS(statement.Cypher),
+			removeWS(s2.Cypher),
+		)
+	}
+
+}
+
+func TestOnlyReturnCount(t *testing.T) {
+	cypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN c
+  `
+
+	statement := NewStatementNoProps(cypher, "fake query")
+	statement.
+		OnlyReturnACount("d.hello")
+
+	newCypher := `
+    MATCH (c:Hello:World)
+    MATCH (d:Hello:World)<-[r:HOLA]-(e:Playa)
+    RETURN COUNT(d.hello) AS _n
+  `
+
+	s2 := NewStatementNoProps(newCypher, "ok")
+	s2.clean()
+
+	if removeWS(statement.Cypher) != removeWS(s2.Cypher) {
+		t.Fatalf("Expected cypher %s but got %s",
+			removeWS(statement.Cypher),
+			removeWS(s2.Cypher),
+		)
+	}
+
+}
