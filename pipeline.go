9c23c314661a8400cf114de055d5af01d66550d0
selim <selim@wearkinetic.com>
2017-03-17 14:03:44 -0400

    added a method to return the n of elts too

--- pipeline.go
+++ pipeline.go
@@ -7,11 +7,31 @@ import "github.com/hippoai/graphgo"
 func (neo *Neo) RequestAndConvert(payload *Payload) (*graphgo.Output, error) {
 
 	response, err := neo.Request(payload)
-
 	if err != nil {
 		return nil, errNeo4JRequest()
 	}
 
 	return Convert(response)
+
+}
+
+// RequestConvertAndGetN
+func (neo *Neo) RequestConvertAndGetN(payload *Payload) (*graphgo.Output, int, error) {
+	response, err := neo.Request(payload)
+	if err != nil {
+		return nil, 0, errNeo4JRequest()
+	}
+
+	out, err := Convert(response)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	n, err := response.GetN()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return out, n, nil
 
 }
